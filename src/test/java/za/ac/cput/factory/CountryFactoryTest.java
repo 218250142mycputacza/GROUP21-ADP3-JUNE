a69c3de0120c08b9b06e1b2a39a0b82342df5b0e
Levi <218250142@mycput.ac.za>
2022-06-12 14:45:10 +0300

    domain and factory

---
+++ src/test/java/za/ac/cput/factory/CountryFactoryTest.java
@@ -0,0 +1,17 @@
+//Name: Levi Williams
+// Student number: 218250142
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.entity.Country;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountryFactoryTest {
+    @Test
+    public void test() {
+        Country country = CountryFactory.createCountry("", "");
+        System.out.println(country);
+        assertNotNull(country);
+    }
+}
