7e6a4af17dccdd192cf17b52038509132e242b95
Nawaaz Amien <219099839@mycput.ac.za>
2022-06-12 13:25:58 +0200

    Question 1 and 2

---
+++ src/test/java/za/ac/cput/factory/StudentAddressFactoryTest.java
@@ -0,0 +1,31 @@
+package za.ac.cput.factory;
+
+/*
+Nawaaz Amien
+219099839
+ */
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.StudentAddress;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentAddressFactoryTest {
+    @Test
+    public void buildWithSuccess()
+    {
+        StudentAddress studentAddress=StudentAddressFactory
+                .build("Test-Id");
+        System.out.println(studentAddress);
+        assertNotNull(studentAddress);
+    }
+    @Test
+    void buildWithError()
+    {
+        Exception exception=assertThrows(IllegalArgumentException.class,()-> StudentAddressFactory.build(null));
+        String exceptionMessage=exception.getMessage();
+        System.out.println(exceptionMessage);
+        assertSame("Student ID is required!",exceptionMessage);
+    }
+
+}
