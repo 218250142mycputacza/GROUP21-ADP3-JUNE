134b7c9a81037cad8a6f4a37e06ba8d5694220ef
Levi <218250142@mycput.ac.za>
2022-06-14 18:39:55 +0300

    commit 2

--- src/main/java/za/ac/cput/factory/CountryFactory.java
+++ src/main/java/za/ac/cput/factory/CountryFactory.java
@@ -1,14 +1,22 @@
 //Name: Levi Williams
-// Student number: 218250142
+// Student Number: 218250142
 package za.ac.cput.factory;
 
 import za.ac.cput.domain.Country;
 
 public class CountryFactory {
-    public static Country createCountry(String id, String name){
-        return new Country.Builder()
-                .setId(id)
-                .setName(name)
-                .build();
+    public static Country build(String countryId, String countryName)
+    {
+        if(countryId ==null ||countryId.isEmpty() )
+            throw new IllegalArgumentException("Country Id is required!");
+        if( countryName ==null||countryName.isEmpty() )
+            throw new IllegalArgumentException("Name is required!");
+        return new Country.Builder().countryId(countryId)
+                .countryName(countryName).build();
+
+    }
+    public static Country.CountryIdentity buildId(Country country)
+    {
+        return new Country.CountryIdentity(country.getCountryId());
     }
 }
