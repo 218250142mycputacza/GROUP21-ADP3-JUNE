0e1bf3dd4b98eab77884296ae973303a23c8a843
Zelino Pestana-218325991 <zelinopestana@gmail.com>
2022-06-14 17:44:28 +0200

    Domain entity, Factory & Repository

--- src/main/java/za/ac/cput/domain/Name.java
+++ src/main/java/za/ac/cput/domain/Name.java
@@ -1,79 +1,90 @@
 package za.ac.cput.domain;
-import javax.persistence.Embeddable;
 
 /* Zelino Pestana 218325991 */
 
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
 @Embeddable
-public class Name {
-
+@IdClass(Name.NameId.class)
+public class Name implements Serializable {
+    @NotNull
+    @Id
     public String firstName;
-    public String middleName;
-    public String lastName;
+    @NotNull public String lastName,middleName;
 
-    public Name (Builder builder){
-        this.firstName = builder.firstName;
-        this.middleName = builder.middleName;
-        this.lastName = builder.lastName;
+    protected Name(){}
+    public Name(Builder builder)
+    {
+        this.firstName=builder.firstName;
+        this.lastName=builder.lastName;
+        this.middleName=builder.middleName;
     }
+    public String getFirstName(){return firstName;}
+    public String getMiddleName(){return middleName;}
+    public String getLastName(){return lastName;}
 
-    public Name() {
+    public static class Builder
+    {
+        public String firstName;
+        public String middleName;
+        public String lastName;
 
+        public Builder firstName(String firstName)
+        {
+            this.firstName=firstName;
+            return this;
+        }
+        public Builder middleName(String middleName)
+        {
+            this.middleName=middleName;
+            return this;
+        }
+        public Builder lastName(String lastName)
+        {
+            this.lastName=lastName;
+            return this;
+        }
+
+        public Builder copy(Name name)
+        {
+            this.firstName=name.firstName;
+            this.middleName=name.middleName;
+            this.lastName=name.lastName;
+            return this;
+        }
+        public Name build(){return new Name(this);}
     }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public static class NameId implements Serializable
+    {
+        public String firstName;
+        public NameId(String firstName){this.firstName=firstName;}
+        protected NameId(){}
+        public String getFirstName(){return firstName;}
     }
-
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
+    @Override
+    public boolean equals(Object o)
+    {
+        if(this==o) return true;
+        if(o==null || getClass()!=o.getClass()) return false;
+        Name name=(Name) o;
+        return firstName.equals(name.firstName);
     }
-
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    @Override
+    public int hashCode(){return Objects.hash(firstName);}
 
     @Override
     public String toString() {
         return "Name{" +
                 "firstName='" + firstName + '\'' +
-                ", middleName='" + middleName + '\'' +
                 ", lastName='" + lastName + '\'' +
+                ", middleName='" + middleName + '\'' +
                 '}';
     }
-
-    public static class Builder{
-
-        public String firstName;
-        public String middleName;
-        public String lastName;
-
-        public Builder setFirstName(String firstName) {
-            this.firstName = firstName;
-            return this;
-        }
-
-        public Builder setMiddleName(String middleName) {
-            this.middleName = middleName;
-            return this;
-        }
-
-        public Builder setLastName(String lastName) {
-            this.lastName = lastName;
-            return this;
-        }
-        public Builder copy (Name name){
-            this.firstName = name.firstName;
-            this.middleName = name.middleName;
-            this.lastName = name.lastName;
-
-            return this;
-
-        }
-        public Name build(){
-            return new Name (this);
-
-        }
-    }
-
 }
--- src/test/java/za/ac/cput/factory/NameFactoryTest.java
+++ src/test/java/za/ac/cput/factory/NameFactoryTest.java
@@ -1,19 +1,26 @@
 package za.ac.cput.factory;
 
-import za.ac.cput.domain.Name;
 import org.junit.jupiter.api.Test;
-//import util.Help;
+import za.ac.cput.domain.Name;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class NameFactoryTest {
-
-
     @Test
-    public void test(){
-        Name name = NameFactory.createName("Zelino", "Sancho", "Pestana");
-        System.out.println(name.toString());
+    public void buildWithSuccess()
+    {
+        Name name=NameFactory.build("Zelino","none","Pestana");
+        System.out.println(name);
         assertNotNull(name);
+    }
+    @Test
+    void buildWithError()
+    {
+        Exception exception=assertThrows(IllegalArgumentException.class,()->NameFactory.build(null,"none","Pestana"));
+        String exceptionMessage=exception.getMessage();
+        System.out.println(exceptionMessage);
+        assertSame("First Name is required",exceptionMessage);
     }
 
 }
