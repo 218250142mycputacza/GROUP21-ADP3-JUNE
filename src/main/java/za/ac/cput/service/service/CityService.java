bb1ea01b07a21d7b1385e5d1df186e5f72bd0423
218290276 <80412082+218290276@users.noreply.github.com>
2022-06-16 17:49:15 +0200

    Entity changes Service and impl

---
+++ src/main/java/za/ac/cput/service/service/CityService.java
@@ -0,0 +1,17 @@
+package za.ac.cput.service.service;
+
+/**
+ * City Service
+ * Aristoteles 218290276
+ * 15/062022
+ */
+import za.ac.cput.domain.City;
+import java.util.List;
+
+public interface CityService extends IService<City,City.CityId> {
+    List<City> findAll();
+    //void deleteById(String id);
+    List<City> findById(String id);
+}
+
+
